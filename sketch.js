49227788b6f8ccacd0183ac4c1ee62b6916318ff
whitedevil0710 <82816110+whitedevil0710@users.noreply.github.com>
2021-06-23 20:51:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine, world;
+
+var paper, ground;
+var leftSide, rightSide, bottom;
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//creating the paper
+	paper = new Paper(100, 600, 10);
+
+	//creating the ground
+    ground = new Ground(400, 680, 800, 20);
+  
+	//creating the dustbin
+	bottom = new Dustbin(610, 660, 100, 20);
+    leftSide = new Dustbin(550, 620, 20, 100);
+    rightSide = new Dustbin(670, 620, 20, 100);
+
+	Engine.run(engine);
+  
+}
+
+function draw() {
+  background(0);
+  
+  Engine.update(engine);
+
+  paper.display();
+  ground.display();
+  leftSide.display();
+  bottom.display();
+  rightSide.display();
+
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW) {
+    Matter.Body.applyForce(paper.body, paper.body.position, {x:15, y: -15})
+  }
+}
+
+
